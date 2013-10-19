8487225315bdb0c9b18e56547c4e063c2cbc7a3b
Andrei Bocan <zmaxor@gmail.com>
2013-10-19 08:38:14 +0300

    Fancy playlist is fancy

--- test/music_library_test.rb
+++ test/music_library_test.rb
@@ -1,60 +1,60 @@
 require 'minitest/unit'
 require 'minitest/autorun'
-require_relative '../music_library.rb'
+require_relative '../player.rb'
 
-class MusicLibraryTest < MiniTest::Unit::TestCase
+class FancyPlaylistTest < MiniTest::Unit::TestCase
   def setup
-    @library = MusicLibrary.new
+    @playlist = FancyPlaylist.new
   end
 
   def test_lets_me_add_a_song
-    assert @library.respond_to? :add_song
+    assert @playlist.respond_to? :add_song
   end
 end
 
-class MusicLibraryWithASongTest < MusicLibraryTest
+class FancyPlaylistWithASongTest < FancyPlaylistTest
   def setup
     super
-    @library.add_song({ :artist => 'U2', :name => 'Some tired song' })
+    @playlist.add_song({ 'artist' => 'U2', 'title' => 'Some tired song' })
   end
 
   def test_returns_the_number_of_songs
-    assert_equal 1, @library.size
+    assert_equal 1, @playlist.size
   end
 
   def test_returns_songs_by_an_artist
-    assert_equal 1, @library.songs_by_artist('U2').length
+    assert_equal 1, @playlist.find_by_artist('U2').length
   end
 
   def test_returns_songs_matching_a_regex
-    assert_equal 0, @library.songs_matching(/foo/).length
-    assert_equal 1, @library.songs_matching(/tired/).length
+    assert_equal 0, @playlist.songs_matching(/foo/).length
+    assert_equal 1, @playlist.songs_matching(/tired/).length
   end
 
   def test_can_find_by_artist_and_name
-    assert_equal 1, @library.find_by_artist('U2').length
-    assert_equal 1, @library.find_by_name('Some tired song').length
-    assert_equal 0, @library.find_by_artist('Rednex').length
-    assert_equal 0, @library.find_by_name('That other song').length
+    assert_equal 1, @playlist.find_by_artist('U2').length
+    assert_equal 1, @playlist.find_by_title('Some tired song').length
+    assert_equal 0, @playlist.find_by_artist('Rednex').length
+    assert_equal 0, @playlist.find_by_title('That other song').length
   end
 
   def test_can_find_by_arbitrary_fields
-    assert @library.respond_to? :find_by_artist
-    assert !@library.respond_to?(:find_by_bitrate)
-    @library.add_song({ :artist => 'Green Day',
-                        :name => 'The one that sounds like that other one',
-                        :bitrate => 192 })
-    assert @library.respond_to?(:find_by_bitrate)
+    @playlist.add_song({ 'artist' => 'Green Day',
+                         'title' => 'The one that sounds like that other one',
+                         'bitrate' => '192' })
+    assert_respond_to @playlist, :find_by_artist
+    assert_respond_to @playlist, :find_by_bitrate
+    assert_equal 1, @playlist.find_by_bitrate(192).length
   end
 
   def test_can_iterate_through_songs
-    assert_equal 1, @library.size
+    assert_equal 1, @playlist.size
     size = 0
-    @library.each { size += 1 }
+    @playlist.each { size += 1 }
     assert_equal 1, size
   end
 
   def test_can_map_over_songs
-    assert_equal ['U2'], @library.map { |s| s[:artist] }
+    assert_equal ['U2'], @playlist.map { |s| s.artist }
   end
 end
