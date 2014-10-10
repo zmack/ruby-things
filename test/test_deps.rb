5e8ea1da50e98b40f5e05977aeb47ee97c75b095
Andrei Bocan <zmaxor@gmail.com>
2014-10-10 20:53:47 +0300

    Add assertion

--- test/test_deps.rb
+++ test/test_deps.rb
@@ -1,5 +1,6 @@
 require 'minitest/unit'
 require 'minitest/autorun'
+require 'stringio'
 
 def _
   flunk "Replace _ with an actual value"
@@ -7,4 +8,20 @@ end
 
 def truthy?(value)
   !!value
+end
+
+def assert_print(expected)
+  temp_io = StringIO.new
+
+  $stdout = temp_io
+
+  yield
+
+  temp_io.rewind
+  result = temp_io.read
+
+  assert_equal expected, result.strip
+
+ensure
+  $stdout = STDOUT
 end
